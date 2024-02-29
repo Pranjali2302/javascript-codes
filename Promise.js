21bcc012c4a9eb6703ae179dad911aebfe6c7fef
Pranjali <37480419+Pranjali2302@users.noreply.github.com>
2024-02-29 18:48:14 +0530

    Update Promise.js

--- Promise.js
+++ Promise.js
@@ -1,10 +1,60 @@
-let promise = new Promise((res,rej)=>{
-  setTimeout(()=>{
-    // res('resolved');
-    rej('rejected');
-  },100)
-})
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, "foo");
+});
+const promise4 = new Promise((resolve, reject) => {
+  const err = new Error("error");
+  reject(err);
+});
+// reject case
+// Promise.all([promise1, promise2, promise4])
+//   .then((values) => {
+//     console.log(values);
+//   })
+//   .catch((e) => {
+//     console.log("error", e.message);
+//   });
+// //fullfill case
+// Promise.all([promise1, promise2, promise3]).then((values) => {
+//   console.log(values);
+// });
 
-promise
-  .then(res=>console.log('res -> ',res))
-  .catch(rej=>console.log('rej -> ',rej))
+// reject case
+Promise.any([promise4])
+  .then((values) => {
+    console.log(values);
+  })
+  .catch((e) => {
+    console.log("error", e.message);
+  });
+//fullfill case
+Promise.any([promise1, promise2, promise3]).then((values) => {
+  console.log(values);
+});
+
+//reject case
+// Promise.allSettled([promise1, promise2, promise4])
+//   .then((values) => {
+//     console.log(values);
+//   })
+//   .catch((e) => {
+//     console.log("error", e.message);
+//   });
+// //fullfill case
+// Promise.allSettled([promise1, promise2, promise3]).then((values) => {
+//   console.log(values);
+// });
+
+//reject case
+// Promise.race([promise1, promise4])
+//   .then((values) => {
+//     console.log(values);
+//   })
+//   .catch((e) => {
+//     console.log("error", e.message);
+//   });
+// //fullfill case
+// Promise.race([promise1, promise2, promise3]).then((values) => {
+//   console.log(values);
+// });
